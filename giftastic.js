6bde460d278bb282ed1c2995ed53a564ec89df93
arusmanukyan <manukyan.arus@gmail.com>
2016-11-02 22:01:02 -0700

    hw6

---
+++ giftastic.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){ 
+
+var fruit = ["mango", "orange", "banana", "pineapple", "watermelon"];
+
+	function createButtons(){
+    // empty out all the buttons
+	$("#buttons").empty();
+  // create var and add user input to fruit array
+  var addFruit = $("#fruit-input").val();
+  fruit.push(addFruit);
+  // create all the buttons in the fruit array
+	for (var i = 0; i < fruit.length; i++){ 		
+		var newButton = $("<button>")
+    newButton.addClass("newFruit");
+    newButton.attr("data-fruit", fruit[i]);
+    newButton.text(fruit[i]);
+
+ 		$("#buttons").append(newButton);
+
+    console.log(buttons);
+
+ 	};
+ };
+  $("#addFruit").on("click", function(){
+    createButtons();
+
+    return false;
+  });
+
+ $(document).on('click', "button", function() {
+     var p = $(this).data('fruit');
+     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  $.ajax({
+      url: queryURL,
+      method: 'GET'
+   })
+     .done(function(response) {
+       	var results = response.data;
+
+       for (var i = 0; i < results.length; i++) {
+       	var gifDiv = $('<div class="item">');
+ 		var rating = results[i].rating;
+
+         var p = $('<p>').text("Rating: " + rating);
+
+         var fruitImage = $('<img>');
+         fruitImage.attr('src', results[i].images.fixed_height.url);
+
+         gifDiv.append(p);
+         gifDiv.append(fruitImage);
+
+         $("#gifsAppearHere").prepend(gifDiv);
+                
+       };
+   	});
+ });
+});
+
+  // need to add data-still to 
+  $('.gif').on('click', function(){
+      var state = $(this).attr("data-state");
+      if (state === 'still') {
+               $(this).attr('src', $(this).attr('data-animate'));
+               $(this).attr('data-state','animate');
+           } else if (state === 'animate') {
+               $(this).attr('src', $(this).attr('data-still'));
+               $(this).attr('data-state','still');
+           };
+  });
